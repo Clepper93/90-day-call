4323d0bf58ecd7c95f9cfc7ddb92c91d2d8b1cda
Megan/Elyott <33584689+Clepper93@users.noreply.github.com>
2018-02-02 14:09:46 -0800

    Updated code comments

--- account-calls-week.py
+++ account-calls-week.py
@@ -8,7 +8,7 @@ with open('accounts.txt', newline='') as inputfile:
     for row in csv.reader(inputfile):
         acctslist.append(row)
 
-#determine the date 90 days from today:
+#determine the date 90-96 days from today:
 todayplus90 = datetime.today() + timedelta(days=90)
 todayplus91 = datetime.today() + timedelta(days=91)
 todayplus92 = datetime.today() + timedelta(days=92)
@@ -20,7 +20,7 @@ todayplus96 = datetime.today() + timedelta(days=96)
 #new list for accounts that match the formula below:
 call_list = []
 
-#format dates, determine if any accounts are 90 days from today's date, append to call_list, print account name and assigned sales rep of all accounts in call_list:
+#format dates, determine if any accounts are 90-96 days from today's date, append to call_list, print account name and assigned sales rep of all accounts in call_list:
 for acct in acctslist:
     acct[0] = acct[0].replace('/',' ')
     acct[0] = datetime.strptime(acct[0], '%m %d %Y')
